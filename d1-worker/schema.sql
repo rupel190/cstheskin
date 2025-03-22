7390b47456a39e3b3bc68a5db875179bb3521e09
rupel <23055682+rupel190@users.noreply.github.com>
2025-03-22 17:12:29 +0100

    update database

--- d1-worker/schema.sql
+++ d1-worker/schema.sql
@@ -3,8 +3,9 @@ CREATE TABLE IF NOT EXISTS skins (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	uuid TEXT UNIQUE NOT NULL,
 	name TEXT NOT NULL,
-	encrypted_name TEXT NOT NULL
-	);
+	encrypted_name TEXT NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 DROP TABLE IF EXISTS skin_images;
 CREATE TABLE IF NOT EXISTS skin_images (
@@ -12,12 +13,13 @@ CREATE TABLE IF NOT EXISTS skin_images (
 	skin_id INTEGER NOT NULL REFERENCES skins(id),
 	stage INTEGER NOT NULL,
 	image_path TEXT NOT NULL
-	);
+);
 
 DROP TABLE IF EXISTS player;
 CREATE TABLE IF NOT EXISTS player (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	uuid TEXT UNIQUE NOT NULL
+	uuid TEXT UNIQUE NOT NULL,
+	name TEXT
 );
 
 DROP TABLE IF EXISTS player_progress;
@@ -25,13 +27,16 @@ CREATE TABLE IF NOT EXISTS player_progress (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	player_id INTEGER NOT NULL REFERENCES player(id),
 	skin_id INTEGER NOT NULL REFERENCES skins(id),
-	current_guess INTEGER DEFAULT 1, -- 1 to 5 getting progressively easier
+	current_stage INTEGER DEFAULT 1, -- 1 to 5 getting progressively easier
 	solved BOOLEAN DEFAULT FALSE,
 	UNIQUE (player_id, skin_id)
 );
 
-insert into skins  (uuid, name, encrypted_name) values
+insert into skins (uuid, name, encrypted_name) values
 	('furzuuid', 'redlangos', 'secretredlangos');
 
 -- INSERT INTO Customers (CustomerID, CompanyName, ContactName)
 -- VALUES (1, 'Alfreds Futterkiste', 'Maria Anders'), (4, 'Around the Horn', 'Thomas Hardy'), (11, 'Bs Beverages', 'Victoria Ashworth'), (13, 'Bs Beverages', 'Random Name');
+--
+-- generate uuid in sqlite!
+-- lower(hex(randomblob(16)))
--- d1-worker/src/index.ts
+++ d1-worker/src/index.ts
@@ -1,25 +1,164 @@
-export interface Env {
-	// If you set another name in the Wrangler config file for the value for 'binding',
-	// replace "DB" with the variable name you defined.
-	devDB: D1Database;
-}
+// src/index.ts
+var index_default = {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+		const { pathname } = url
+		const { searchParams } = url
 
-export default {
-	async fetch(request, env): Promise<Response> {
-		const { pathname } = new URL(request.url);
-
+		// (Debug) Get skin by name
 		if (pathname === "/api/skins") {
-			// If you did not use `DB` as your binding name, change it here
+			try {
+				let name = searchParams.get("name");
+				const query = env.devDB.prepare("SELECT * FROM skins WHERE name = ?").bind(name);
+				const { results } = await query.all();
+				// console.log("Query results:", results);
+				return Response.json(results);
+			} catch (error) {
+				console.error("Error executing query:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		// Get skin by uuid
+		if (pathname === "/api/skins") {
+			try {
+				const uuid = searchParams.get("uuid");
+				const query = env.devDB.prepare("SELECT * FROM skins WHERE uuid = ?").bind(uuid);
+				const { results } = await query.all();
+				return Response.json(results);
+			} catch (error) {
+				console.error("Error executing query:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		// Get all skin uuids
+		if (url.pathname === "/api/skins/uuids") {
+			// Fetch all skin UUIDs
 			const { results } = await env.devDB.prepare(
-				"SELECT * FROM skins where name = ?",
-			)
-				.bind("redlangos")
-				.all();
+				"SELECT uuid FROM skins ORDER BY created_at ASC"
+			).all();
 			return Response.json(results);
 		}
 
-		return new Response(
-			"Call /api/skins to get a skin per name",
-		);
-	},
-} satisfies ExportedHandler<Env>;
+		// Insert new player
+		if (request.method === "GET" && url.pathname === "/api/player/new") {
+			try {
+				const name = searchParams.get("name");
+				const playerUuid = crypto.randomUUID(); // Generate unique UUID
+
+				if (!name) {
+					env.devDB
+						.prepare("INSERT INTO players (uuid) VALUES (?)")
+						.bind(playerUuid)
+						.run() // Use .run() for INSERT
+				} else {
+					env.devDB
+						.prepare("INSERT INTO players (uuid, name) VALUES (?, ?)")
+						.bind(playerUuid, name)
+						.run()
+				}
+				return Response.json({ message: "Player created", uuid: playerUuid });
+			} catch (error) {
+				console.error("Error inserting player:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		// (DEBUG) Get player by uuid
+		if (url.pathname === "/api/player") {
+			try {
+				const uuid = searchParams.get("uuid");
+				const { results } = await env.devDB.prepare(
+					"SELECT * FROM player WHERE uuid = ?")
+					.bind(uuid)
+					.all();
+				return Response.json(results);
+
+			} catch (error) {
+				console.error("Error getting player by uuid:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		// Get player progress per uuid
+		if (request.method === "GET" && url.pathname === "/api/progress") {
+			try {
+				const uuid = searchParams.get("uuid");
+				const { results } = await env.devDB.prepare(
+					"SELECT skin, current_stage, solved FROM player_progress WHERE player_id = (SELECT id FROM players WHERE uuid = ?)")
+					.bind(uuid)
+					.all();
+				return Response.json(results);
+
+			} catch (error) {
+				console.error("Error getting player progress by uuid:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		// Get image for skin and stage
+		if (url.pathname === "/api/image/") {
+			try {
+				const { uuid, current_stage } = await request.json();
+				const { results } = await env.devDB.prepare(
+					"SELECT image_path FROM skin_images WHERE skin_id = (SELECT id FROM skins WHERE uuid = ?) AND stage = ?")
+					.bind(uuid, current_stage)
+					.all();
+				return Response.json(results);
+
+			} catch (error) {
+				console.error("Error getting image by skin and stage:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		// 💷 Guess 🎰 the 🎰 skin 💷
+		if (url.pathname === "/api/guess/") {
+			try {
+				const { uuid, guess_input } = await request.json();
+
+				const { results } = await env.devDB.prepare(
+					"SELECT count(*) FROM skins WHERE uuid = ? AND name = ?")
+					.bind(uuid, guess_input)
+					.first();
+				return Response.json(results);
+
+			} catch (error) {
+				console.error("Error getting image by skin and stage:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		// Save player progress
+		if (request.method === "POST" && url.pathname === "/api/progress") {
+			try {
+				const { player_uuid, skin_uuid, current_stage, solved } = await request.json();
+
+				env.devDB
+					.prepare(`
+							INSERT INTO player_progress (player_id, skin_id, current_stage, solved)
+							VALUES (
+									(SELECT id FROM players WHERE uuid = ?),
+									(SELECT id FROM skins WHERE uuid = ?),
+									?, ?
+							)
+							ON CONFLICT(player_id, skin_id)
+							DO UPDATE SET
+									current_stage = excluded.current_stage,
+									solved = excluded.correct`)
+					.bind(player_uuid, skin_uuid, current_stage, solved)
+					.run() // Use .run() for INSERT
+				return Response.json({ message: "Player progress updated" });
+			} catch (error) {
+				console.error("Error inserting player:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+	}
+};
+export {
+	index_default as default
+};
+//# sourceMappingURL=index.js.map
